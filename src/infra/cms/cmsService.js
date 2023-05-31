68e35d610468604941fc223525eba9b8d6979067
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-05-31 10:14:27 -0300

    fiz uma função para fazer querys no datocms

---
+++ src/infra/cms/cmsService.js
@@ -0,0 +1,30 @@
+const TOKEN = process.env.DATO_TOKEN;
+
+export async function cmsService({ query }) {
+  try {
+    const pageContentResponse = await fetch('https://graphql.datocms.com/', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${TOKEN}`,
+      },
+      body: JSON.stringify({
+        query,
+      }),
+    }).then(async (res) => {
+      const body = await res.json();
+      console.log('body', body);
+      if (!body.errors) return body;
+
+      throw new Error(JSON.stringify(body));
+    });
+
+    // console.log('pageContentResponse', pageContentResponse);
+
+    return {
+      data: pageContentResponse.data,
+    };
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
