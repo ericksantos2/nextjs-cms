1c2acb6b3b3e5644caafd76863e1e050659ab05a
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-07 11:22:01 -0300

    adicionei uma global query

--- src/infra/cms/cmsService.js
+++ src/infra/cms/cmsService.js
@@ -1,8 +1,20 @@
 const TOKEN = process.env.DATO_TOKEN;
 
-export async function cmsService({ query }) {
+const globalQuery = `
+  query {
+    globalFooter {
+      description
+    }
+  }
+`
+
+const BASE_ENDPOINT = 'https://graphql.datocms.com/';
+const PREVIEW_ENDPOINT = BASE_ENDPOINT + 'preview';
+
+export async function cmsService({ query, preview = false }) {
+  const ENDPOINT = preview ? PREVIEW_ENDPOINT : BASE_ENDPOINT;
   try {
-    const pageContentResponse = await fetch('https://graphql.datocms.com/', {
+    const pageContentResponse = await fetch(ENDPOINT, {
       method: 'POST',
       headers: {
         'Content-type': 'application/json',
@@ -13,16 +25,32 @@ export async function cmsService({ query }) {
       }),
     }).then(async (res) => {
       const body = await res.json();
-      console.log('body', body);
       if (!body.errors) return body;
-
       throw new Error(JSON.stringify(body));
     });
 
-    // console.log('pageContentResponse', pageContentResponse);
+    const globalContentResponse = await fetch(ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${TOKEN}`,
+      },
+      body: JSON.stringify({
+        query: globalQuery,
+      }),
+    }).then(async (res) => {
+      const body = await res.json();
+      if (!body.errors) return body;
+      throw new Error(JSON.stringify(body));
+    });
 
     return {
-      data: pageContentResponse.data,
+      data: {
+        ...pageContentResponse.data,
+        globalContent: {
+          ...globalContentResponse.data,
+        }
+      },
     };
   } catch (err) {
     throw new Error(err.message);
