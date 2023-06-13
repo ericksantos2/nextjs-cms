65ee2ae61267d738449f317402e202eab64ccf09
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-13 20:26:39 -0300

    adicionei uma nova section do dato

--- src/components/cmsSections/index.js
+++ src/components/cmsSections/index.js
@@ -1,11 +1,13 @@
-import { Footer } from "../commons/Footer";
-import { Menu } from "../commons/Menu";
-import { PageHomeHeroSection } from "./PageHomeHeroSection";
-import { SEOBlock } from "./SeoBlock";
+import { Footer } from '../commons/Footer';
+import { Menu } from '../commons/Menu';
+import { PageFAQDisplayQuestionsSection } from './PageFAQDisplayQuestionsSection';
+import { PageHomeHeroSection } from './PageHomeHeroSection';
+import { SEOBlock } from './SeoBlock';
 
 export const cmsSections = {
   CommonSeoBlockRecord: SEOBlock,
   CommonMenuRecord: Menu,
   PagehomeHerosectionRecord: PageHomeHeroSection,
-  CommonFooterRecord: Footer
-}
+  CommonFooterRecord: Footer,
+  PagefaqDisplayquestionSectionRecord: PageFAQDisplayQuestionsSection,
+};
