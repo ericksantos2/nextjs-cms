6788439675a04c650bfdb37dd370aefefcf12b09
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-13 19:57:49 -0300

    agora a pagehome tem seu conteudo renderizado pelo cmssectionrender e as coisas que estavam aqui foram movidas para o datocms

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -2,71 +2,74 @@ import Head from 'next/head';
 import { Menu } from '../../components/commons/Menu';
 import { Footer } from '../../components/commons/Footer';
 import { theme, Box, Button, Text, Image } from '../../theme/components';
+import { cmsService } from '../../infra/cms/cmsService.js';
+import { pageHOC } from '../../components/wrappers/pageHOC.js';
+import { CMSSectionRender } from '../../infra/cms/CMSSectionRender';
 
-export function getStaticProps() {
+export async function getStaticProps({ preview }) {
+  const { data: cmsContent } = await cmsService({
+    query: `
+      query {
+        pageHome {
+          pageContent {
+            section {
+              componentName: __typename
+              ... on CommonSeoBlockRecord {
+                id
+                title
+              }
+              ... on CommonMenuRecord {
+                id
+              }
+              ... on CommonFooterRecord {
+                id
+              }
+              ... on PagehomeHerosectionRecord {
+                id
+                title
+                description
+                ctatext
+                ctalink
+              }
+            }
+          }
+        }
+      }
+    `,
+    preview,
+  });
+
   return {
-    props: {}
-  }
+    props: {
+      cmsContent,
+    },
+  };
 }
 
 function HomeScreen() {
   return (
-    <>
-      <Head>
-        <title>Home - Alura</title>
-      </Head>
-
-      <Menu />
-
-      <Box
-        tag="main"
-        styleSheet={{
-          flex: 1,
-          paddingTop: theme.space.x20,
-          paddingHorizontal: theme.space.x4,
-          background: `linear-gradient(${theme.colors.primary.x900}, ${theme.colors.primary.x700})`,
-          color: theme.colors.neutral.x000,
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: {
-            xs: 'column',
-            md: 'row',
-          },
-          flexWrap: 'nowrap',
-          justifyContent: 'space-evenly',
-        }}
-      >
-        <Box
-          styleSheet={{
-            maxWidth: '450px'
-          }}
-        >
-          <Text tag="h1" variant="display1">
-            Mergulhe em Tecnologia!
-          </Text>
-          <Text tag="p" variant="body1">
-            Você vai estudar, praticar, discutir e se aprofundar em uma plataforma que respira tecnologia.
-          </Text>
-          <Button href="/faq" colorVariant="neutral">
-            Principais dúvidas
-          </Button>
-        </Box>
-
-        <Image
-          src="https://www.alura.com.br/assets/img/home/homeNova/ilustra-alura-escafandro.1647533643.svg"
-          styleSheet={{
-            maxWidth: {
-              xs: '200px',
-              sm: 'initial',
-            },
-            marginVertical: theme.space.x10,
-          }}
-        />
-      </Box>
-
-      <Footer />
-    </>
+    <CMSSectionRender pageName="pageHome" />
   )
 }
 
-export default HomeScreen;
+// function HomeScreen() {
+//   return (
+//     <>
+//       {/* Common SEO Block */}
+//       <Head>
+//         <title>Home - Alura</title>
+//       </Head>
+
+//       {/* Menu */}
+//       <Menu />
+
+//       {/* HEROSECTION */}
+      
+
+//       {/* Footer */}
+//       <Footer />
+//     </>
+//   );
+// }
+
+export default pageHOC(HomeScreen);
