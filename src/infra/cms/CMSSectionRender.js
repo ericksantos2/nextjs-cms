a1f0503d556a4c7b7ece9efd872e78a25b928b30
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-13 20:28:32 -0300

    se não houver um componente, retornará null

--- src/infra/cms/CMSSectionRender.js
+++ src/infra/cms/CMSSectionRender.js
@@ -4,7 +4,10 @@ import { getCMSContent } from "./CMSProvider"
 export function CMSSectionRender({ pageName }) {
   const sections = getCMSContent(`${pageName}.pageContent[0].section`);
   return sections.map((sectionProps) => {
-    const Component = cmsSections[sectionProps.componentName]
+    const Component = cmsSections[sectionProps.componentName];
+
+    if(!Component) return null;
+
     return <Component key={sectionProps.id} {...sectionProps} />
   })
 }
