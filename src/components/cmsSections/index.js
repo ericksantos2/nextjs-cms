d579abeed054febc7865666f2e1ac26a4f797056
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-13 19:50:47 -0300

    adicionei um objeto cmsSections com os itens do datocms

---
+++ src/components/cmsSections/index.js
@@ -0,0 +1,11 @@
+import { Footer } from "../commons/Footer";
+import { Menu } from "../commons/Menu";
+import { PageHomeHeroSection } from "./PageHomeHeroSection";
+import { SEOBlock } from "./SeoBlock";
+
+export const cmsSections = {
+  CommonSeoBlockRecord: SEOBlock,
+  CommonMenuRecord: Menu,
+  PagehomeHerosectionRecord: PageHomeHeroSection,
+  CommonFooterRecord: Footer
+}
